a371a9007102c0af594b7c597c10bf842ed96e51
Jakob <jakso@itu.dk>
2025-08-28 10:59:05 +0200

    exercise 1.1

--- PSD_week1/PSD_week1/Program.fs
+++ PSD_week1/PSD_week1/Program.fs
@@ -1,2 +1,73 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿(* Programming language concepts for software developers, 2010-08-28 *)
+
+(* Evaluating simple expressions with variables *)
+
+// module Intro2
+
+(* Association lists map object language variables to their values *)
+
+let env = [("a", 3); ("c", 78); ("baf", 666); ("b", 111)]
+
+let emptyenv = [](* the empty environment *)
+
+let rec lookup env x =
+    match env with 
+    | []        -> failwith (x + " not found")
+    | (y, v)::r -> if x=y then v else lookup r x
+
+let cvalue = lookup env "c"
+
+
+(* Object language expressions with variables *)
+
+type expr = 
+  | CstI of int
+  | Var of string
+  | Prim of string * expr * expr
+
+let e1 = CstI 17
+
+let e2 = Prim("+", CstI 3, Var "a")
+
+let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a")
+
+(* our expression *)
+let e4 = Prim("min", CstI 3, CstI 7)
+let e5 = Prim("min", CstI 7, CstI 3)
+let e6 = Prim("max", CstI 3, CstI 7)
+let e7 = Prim("max", CstI 7, CstI 3)
+let e8 = Prim("==", CstI 3, CstI 7)
+let e9 = Prim("==", CstI 7, CstI 7)
+
+
+
+(* Evaluation within an environment *)
+
+let rec eval e (env : (string * int) list) : int =
+    match e with
+    | CstI i            -> i
+    | Var x             -> lookup env x 
+    | Prim (ope, e1, e2) ->
+        let a = eval e1 env
+        let b = eval e2 env
+        match ope with
+        | "+" -> a+b 
+        | "*" -> a*b 
+        | "-" -> a-b
+        | "min" -> if a < b then a else b
+        | "max" -> if a > b then a else b
+        | "==" -> if a = b then 1 else 0
+        | _ -> failwith "unknown operator"
+
+let e1v  = eval e1 env
+let e2v1 = eval e2 env
+let e2v2 = eval e2 [("a", 314)]
+let e3v  = eval e3 env
+
+(* example expressions 1.1 ii*)
+let e4v = eval e4 env
+let e5v = eval e5 env
+let e6v = eval e6 env
+let e7v = eval e7 env
+let e8v = eval e8 env
+let e9v = eval e9 env
