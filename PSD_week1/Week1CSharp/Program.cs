eb4e55e721fccda77b399136449e88414d5b536c
Jacob <spon@itu.dk>
2025-08-29 13:00:35 +0200

    Skelet

--- PSD_week1/Week1CSharp/Program.cs
+++ PSD_week1/Week1CSharp/Program.cs
@@ -1,4 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+public abstract class Expr
+{
+    public override string ToString()
+    {
+        //return 
+    }
+}
 
-Console.WriteLine("Hello, World!");
+public class CstI : Expr
+{
+    
+}
+
+public class Var : Expr
+{
+
+}
+
+public abstract class Binop : Expr
+{
+
+}
+
+public class Add : Binop
+{
+
+}
+
+public class Mul : Binop
+{
+
+}
+
+public class Sub : Binop
+{
+
+}
+
+public class AccessExample
+{
+    public static void Main(string[] args)
+    {
+        Object[] env = [("a", 3), ("c", 78), ("baf", 666), ("b", 111)];
+        Console.WriteLine("Hello World!");
+    }
+}
+
 
