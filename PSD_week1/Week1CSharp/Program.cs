13e9ec7888eba67ee6b88c339d47e86c67964029
Jacob <spon@itu.dk>
2025-08-29 14:41:11 +0200

    1.4 really done ^^

--- PSD_week1/Week1CSharp/Program.cs
+++ PSD_week1/Week1CSharp/Program.cs
@@ -5,20 +5,22 @@ public abstract class Expr
 
     public abstract override string ToString();
 
+    public abstract Expr Simplify();
 }
 
 public class CstI(int i) : Expr
 {
     protected int I = i;
 
+    public int GetValue => I;
+    
     public override int Eval(Dictionary<string,int> env) {
         return I;
     }
 
-    public override string ToString()
-    {
-        return I.ToString();
-    }
+    public override string ToString() => I.ToString();
+
+    public override Expr Simplify() => this;
 }
 
 public class Var (string str) : Expr
@@ -29,10 +31,9 @@ public class Var (string str) : Expr
         return env.ContainsKey(Name) ? env[Name] : 0;
     }
 
-    public override string ToString()
-    {
-        return Name;
-    }
+    public override string ToString() => Name;
+    
+    public override Expr Simplify() => this;
 }
 
 public abstract class Binop(Expr e1, Expr e2) : Expr
@@ -51,6 +52,18 @@ public class Add(Expr e1, Expr e2) : Binop(e1, e2)
     {
         return E1 + " + " + E2;
     }
+
+    public override Expr Simplify()
+    {
+        var e1 = E1.Simplify();
+        var e2 = E2.Simplify();
+
+        if (e1 is CstI c1 && c1.GetValue == 0) return e2;
+
+        if (e2 is CstI c2 && c2.GetValue == 0) return e1;
+        
+        return new Add(e1, e2);
+    }
 }
 
 public class Mul(Expr e1, Expr e2) : Binop(e1, e2)
@@ -64,6 +77,22 @@ public class Mul(Expr e1, Expr e2) : Binop(e1, e2)
     {
         return "(" + E1 + ")" + " * " + "(" + E2 + ")" ;
     }
+    
+    public override Expr Simplify()
+    {
+        var e1 = E1.Simplify();
+        var e2 = E2.Simplify();
+
+        if (e1 is CstI c1 && c1.GetValue == 1) return e2;
+
+        if (e2 is CstI c2 && c2.GetValue == 1) return e1;
+        
+        if (e1 is CstI c3 && c3.GetValue == 0) return new CstI(0);
+        
+        if (e2 is CstI c4 && c4.GetValue == 0) return new CstI(0);
+        
+        return new Mul(e1, e2);
+    }
 }
 
 public class Sub(Expr e1, Expr e2) : Binop(e1, e2)
@@ -76,6 +105,18 @@ public class Sub(Expr e1, Expr e2) : Binop(e1, e2)
     public override string ToString()
     {
         return E1 + " - " + E2;
+    }
+    
+    public override Expr Simplify()
+    {
+        var e1 = E1.Simplify();
+        var e2 = E2.Simplify();
+
+        if (e1.Equals(e2)) return new CstI(0);
+
+        if (e2 is CstI c2 && c2.GetValue == 0) return e1;
+        
+        return new Sub(e1, e2);
     }
 }
 
@@ -96,6 +137,30 @@ public class AccessExample
         newEnv.Add("x", 1);
         newEnv.Add("y", 2);
         Console.WriteLine(e4.Eval(newEnv));
+        
+        Expr zeroPlus = new Add(new CstI(0), new Var("a"));
+        Expr plusZero = new Add(new Var("b"), new CstI(0));
+        Expr oneTimes = new Mul(new CstI(1), new Var("c"));
+        Expr timesOne = new Mul(new Var("d"), new CstI(1));
+        Expr zeroTimes = new Mul(new CstI(0), new Var("e"));
+        Expr timesZero = new Mul(new Var("f"), new CstI(0));
+        Expr minusZero = new Sub(new Var("g"), new CstI(0));
+        Expr self = new Add(new Var("h"), new CstI(0));
+        Expr minusSelf = new Sub(self, self);
+
+        Expr mega =
+            new Sub(
+                new Add(
+                    new Add(zeroPlus, plusZero),
+                    new Add(new Add(oneTimes, timesOne),
+                        new Add(new Add(zeroTimes, timesZero), minusZero))
+                ),
+                new CstI(0)
+            );
+
+        var e6 = new Add(mega, minusSelf);
+        Console.WriteLine(e6);
+        Console.WriteLine(e6.Simplify());
     }
 }
 
