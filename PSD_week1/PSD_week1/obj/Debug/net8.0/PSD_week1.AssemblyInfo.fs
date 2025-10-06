e7d486e6ddba129290a4e6982afc4af9d4ab745c
Alex Tilgreen Mogensen <alext@itu.dk>
2025-10-06 13:59:04 +0200

    6.4 text

--- PSD_week1/PSD_week1/obj/Debug/net8.0/PSD_week1.AssemblyInfo.fs
+++ PSD_week1/PSD_week1/obj/Debug/net8.0/PSD_week1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("PSD_week1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cda83daa3fcd095e485b7e520d3bcff31ce4087")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb31397d39c1987d052eaa488cc472d86aa1bf1")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("PSD_week1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("PSD_week1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
--- PSD_week2/src/fsharp/FsLexYacc.Runtime.11.3.0/fable/obj/Debug/netstandard2.0/FsLexYacc.Runtime.AssemblyInfo.fs
+++ PSD_week2/src/fsharp/FsLexYacc.Runtime.11.3.0/fable/obj/Debug/netstandard2.0/FsLexYacc.Runtime.AssemblyInfo.fs
@@ -11,7 +11,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyDescriptionAttribute("Runtime for FsLex/FsYacc lexer/parser generation tools")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cda83daa3fcd095e485b7e520d3bcff31ce4087")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb31397d39c1987d052eaa488cc472d86aa1bf1")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FsLexYacc.Runtime")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FsLexYacc.Runtime")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
